f9bd4f5b4d39a553dacfe987bef87fbbbc48dba1
sircatalyst <temibami@gmail.com>
2019-06-18 12:15:55 +0100

    added ninjas component

---
+++ src/Ninjas.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>List of ninjas</h1>
+    <ul>
+        <li v-for="ninja in ninjas"> {{ninja}}</li>
+    </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['yoshi', 'mario', 'ryu']
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+  h1{
+    color:green;
+  }
+</style>
