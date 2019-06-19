64bf54974e764de62242ab020a7fc3c6e16f522f
sircatalyst <temibami@gmail.com>
2019-06-19 11:31:05 +0100

    filters

--- src/main.js
+++ src/main.js
@@ -29,6 +29,14 @@ Vue.directive('theme', {
   }
 })
 
+//filters
+Vue.filter('to-uppercase', function (value) {
+  return value.toUpperCase();
+})
+
+Vue.filter('snippet', function (value) {
+  return value.slice(0, 100) + '...';
+})
 export const bus = new Vue();
 
 new Vue({
